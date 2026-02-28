5728f2b4eef5d5f65089d851441d10e39a23e144
Shrinidhi <shrinidhipoojary616@gmail.com>
2026-02-28 16:35:55 +0530

    added updateRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,11 +2,11 @@ const Recipe = require("../models/Recipe");
 
 // ADD RECIPE
 exports.addRecipe = async (req, res) => {
-  const { title, ingredients, description, instructions, cookTime, servings, image } = req.body;
+  const { title, ingredients, description, instructions, cookingTime, servings, image } = req.body;
 
   try {
     // Basic validation
-    if (!title || !ingredients || !instructions || !cookTime) {
+    if (!title || !ingredients || !instructions || !cookingTime) {
       return res.status(400).json({ message: "Please fill all required fields" });
     }
 
@@ -15,7 +15,7 @@ exports.addRecipe = async (req, res) => {
       description,
       ingredients,
       instructions,
-      cookTime,
+      cookingTime,
       servings,
       image,
       createdBy: req.user._id, // comes from auth middleware
@@ -23,6 +23,7 @@ exports.addRecipe = async (req, res) => {
 
     res.status(201).json(recipe);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -39,7 +40,7 @@ exports.getrecipeByID= async(req,res)=>{
   try{
     const recipe=await Recipe.findById(req.params.id).populate("createdBy","name email");
     if(!recipe){
-      return res.status(404).jsoon({message:"Recipe not found"});
+      return res.status(404).json({message:"Recipe not found"});
     }
     res.json(recipe);
 
@@ -48,4 +49,50 @@ exports.getrecipeByID= async(req,res)=>{
     res.status(500).json({message:"server error"});
 
   }
-}
+};
+exports.deleteRecipe= async(req,res)=>{
+  try{
+    const recipe=await Recipe.findById(req.params.id);
+    if(!recipe){
+      return res.status(404).json({message:"Recipe not found"});
+    }
+    if(recipe.createdBy.toString() !== req.user._id.toString()){
+      return res.status(401).json({message:"Not authorized to delete this recipe"});
+    }
+    await recipe.deleteOne();
+    res.json({message:"recipe deleted successfully"});
+
+  }
+  catch(error){
+    // console.log(error);
+    res.status(500).json({message:"server error"});
+
+  }
+};
+exports.updateRecipe = async(req,res)=>{
+  try{
+    const recipe= await Recipe.findById(req.params.id);
+    if(!recipe){
+      return res.status(404).json({message:"recipe not found"});
+
+    }
+    //check ownership
+    if(recipe.createdBy.toString()!== req.user._id.toString()){
+      return res.status(401).json({message:"not authorised to update this recipe"})
+    }
+    //update recipe
+    recipe.title=req.body.title||recipe.title;
+    recipe.ingredients=req.body.ingredients||recipe.ingredients;
+    recipe.instructions=req.body.instructions||recipe.instructions;
+    recipe.cookingTime=req.body.cookingTime||recipe.cookingTime;
+    recipe.servings=req.body.servings||recipe.servings;
+    recipe.image=req.body.image||recipe.image;
+
+    const updatedRecipe= await recipe.save();
+    res.json(updatedRecipe);
+  }
+  catch(error){
+    res.status(500).json({message:"server error"})
+
+  }
+};
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { addRecipe,getAllRecipes, getrecipeByID } = require("../controllers/recipeController");
+const { addRecipe,
+        getAllRecipes,
+        getrecipeByID,
+        deleteRecipe,
+        updateRecipe
+      } = require("../controllers/recipeController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, addRecipe);
 router.get("/",getAllRecipes);
 router.get("/:id",getrecipeByID);
+router.put("/:id",protect,updateRecipe);
+router.delete("/:id",protect,deleteRecipe);
 module.exports = router;
